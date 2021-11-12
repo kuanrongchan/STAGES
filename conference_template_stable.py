3f00b299edb10b50df1594b2e074d5fbec2e3650
kohcl17 <91475711+kohcl17@users.noreply.github.com>
2021-11-12 11:16:19 +0800

    Update conference_template_stable.py

--- conference_template_stable.py
+++ conference_template_stable.py
@@ -81,7 +81,7 @@ if len(df_query) != 0:
             df_names.append(head)
 else:
     testdata = st.experimental_memo(pd.read_csv)("demo_dataframe.csv", index_col=0)
-    testname = "demo"
+    testname = "Demo"
     df_dict[testname] = testdata
     df_names.append(testname)
 
