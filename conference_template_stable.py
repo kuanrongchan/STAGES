ee34346b652c419c1c76445eac931615b778c170
josg90 <91474827+josg90@users.noreply.github.com>
2021-11-12 14:59:26 +0800

    Update conference_template_stable.py

--- conference_template_stable.py
+++ conference_template_stable.py
@@ -798,8 +798,8 @@ def execute_enrichr(genelist, select_dataset, use_degs=False):
             fig.add_trace(go.Bar(x=toplot_down['-logP'], y=toplot_down.index,
                                  orientation='h', marker_color="#636EFA"),
                           row=2, col=1)
-            fig.update_layout(yaxis={'tickmode':'linear'})
-            fig.update_yaxes(dtick=0) 
+            fig.update_yaxes(tickmode='linear',tick0=0, dtick=0)
+
 
         elif data_up_trunc is not None and data_down_trunc is None:
             toplot_up = data_up_sig.sort_values("-logP", ascending=True).tail(10)
